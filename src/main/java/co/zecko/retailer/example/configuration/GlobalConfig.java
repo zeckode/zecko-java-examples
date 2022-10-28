87adc9be1cc67c4af066ab1ad88e43b2695dc937
Jai Malani <71269258+jai38@users.noreply.github.com>
2022-10-28 21:38:00 +0530

    collectionClient getAll added

--- src/main/java/co/zecko/retailer/example/configuration/GlobalConfig.java
+++ src/main/java/co/zecko/retailer/example/configuration/GlobalConfig.java
@@ -1,14 +1,15 @@
 package co.zecko.retailer.example.configuration;
 
+import co.zecko.retailer.client.httpClient.Zecko;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GlobalConfig {
 
-    public String zeckoAccessToken;
+    public final Zecko zecko;
 
     public GlobalConfig(@Value("${zecko.access.token}") String accessToken) {
-        this.zeckoAccessToken = accessToken;
+        this.zecko = new Zecko(accessToken);
     }
 }
--- src/main/java/co/zecko/retailer/example/service/CollectionService.java
+++ src/main/java/co/zecko/retailer/example/service/CollectionService.java
@@ -1,30 +1,28 @@
 package co.zecko.retailer.example.service;
 
-import co.zecko.retailer.client.httpClient.ZeckoCollectionClient;
+import co.zecko.retailer.client.httpClient.CollectionClient;
+import co.zecko.retailer.client.httpClient.Zecko;
 import co.zecko.retailer.common.pojo.collection.CollectionsData;
 import co.zecko.retailer.example.configuration.GlobalConfig;
 import co.zecko.retailer.exception.BaseException;
 import java.io.IOException;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CollectionService {
 
-    final String zeckoAccessToken;
+    final Zecko zecko;
 
     public CollectionService(GlobalConfig globalConfig) {
-        this.zeckoAccessToken = globalConfig.zeckoAccessToken;
+        this.zecko = globalConfig.zecko;
     }
 
     public CollectionsData findAll(String after, String before)
         throws IOException, InterruptedException, BaseException {
 
-        return new ZeckoCollectionClient().findAll(after, before, zeckoAccessToken);
+        return zecko.collectionClient.findAll(after, before);
     }
 }
